3dc4c1ef4be8eeb285a3dccd5bfd00668d6fdc95
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-03-02 01:54:34 +0200

    homePage

--- js/app.js
+++ js/app.js
@@ -17,23 +17,24 @@ function Book_data(name, imgPath, image, description, price, link) {
   };
   return object;
 }
+
 // Book Categories :
 const front_end_books = [
-  new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
+  new Book_data('front_end_books', 'https://www.google.com', '../images/HTML5CSS3.JPG', '', '', 'http://download1594.mediafire.com/9aivyci7egog/g98232patobhbk8/HTML5+CSS3+For+The+Real+World+2nd+Edition.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/how-to-code-in-html.png', '', '', 'http://download1647.mediafire.com/jvtaqfpzxsag/s0rp9wca36i5om7/CodeHTML%26CSS.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/Bootstrap-A-SitePoint-Anthology-1.png', '', '', 'http://download943.mediafire.com/lqyvultvcmsg/ogt9teytxflwxcp/bootstrap-ant-1_compress.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/javascript.jpg', '', '', 'http://download1587.mediafire.com/8896j3lffqkg/p6ryrl8xrtn5w31/Learning+Three+Js+The+Javascri%27%2B%27pt+3d+Library+For+Web+Gl.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/JQuery.png', '', '', 'http://download1509.mediafire.com/ux36ojsvwtdg/wxr3vuoizfscw1h/Dominando+JavaScript+com+jQuery+Book+of+2012+Year.pdf')
   , new Book_data('front_end_books', 5, '', '', '', '/pdf-folder/book1.pdf')
   , new Book_data('front_end_books', 5, '', '', '', '/pdf-folder/book1.pdf')
   , new Book_data('front_end_books', 5, '', '', '', '/pdf-folder/book1.pdf')
 ];
 const back_end_books = [
-  new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
-  , new Book_data('front_end_books', 'https://www.google.com', '../images/A Tour of C++.jpg', '', '', '../book1.pdf')
+  new Book_data('front_end_books', 'https://www.google.com', '../images/pythonhardway.jpg', '', '', 'https://download1503.mediafire.com/3ot725cs88rg/fxlxksg556m6irf/Learn+Python+The+Hard+Way+3rd+Edition.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/pythonprogramming.jpg', '', '', 'https://download1503.mediafire.com/yvn5g90ojvpg/wmwg2yers1t894n/Python+Programming.++Python+Programming+for+Beginners%2C+Python+Programming+for+Intermediates+%28+PDFDrive+%29.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/Java.jpg', '', '', 'https://download1349.mediafire.com/4xk88zz6yesg/omdbdraebmulnmm/Java+for+Absolute+Beginners_+Learn+to+Program+the+Fundamentals+the+Java+9%2B+Way+%28+PDFDrive+%29.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/php.jpg', '', '', 'https://download1654.mediafire.com/bcodrvn7vl9g/4qfedd8g40sxk3b/Pro+PHP+and+jQuery%2C+2nd+Edition_+Take+your+dynamic+PHP+7+apps+to+the+next+level+by+adding+a+JavaScript+layer+using+jQuery+%28+PDFDrive+%29.pdf')
+  , new Book_data('front_end_books', 'https://www.google.com', '../images/Net.jpg', '', '', 'https://download1507.mediafire.com/q0ehvdwr7yyg/ly59rvjqsf262lk/Applied+Cryptography+in+.NET+and+Azure+Key+Vault_+A+Practical+Guide+to+Encryption+in+.NET+and+.NET+Core+%28+PDFDrive+%29.pdf')
 
   , new Book_data('back_end_book', 5, '', '', '', '/pdf-folder/book1.pdf')
   , new Book_data('back_end_book', 5, '', '', '', '/pdf-folder/book1.pdf')
@@ -43,6 +44,7 @@ const back_end_books = [
 const front_end_ul = document.getElementById('front_end_book');
 const back_end_ul = document.getElementById('back_end_book');
 // front end book
+
 for (let i = 0; i < 5; i++) {
   // create Book card element --> 8 element
   const book_li = document.createElement('li');
@@ -66,12 +68,12 @@ for (let i = 0; i < 5; i++) {
   book_a.href = front_end_books[i].imgPath;
   book_a.target = '_blank';
   book_img.src = front_end_books[i].image;
-  span_button1.textContent = 'Read';
+  span_button1.textContent = 'Description';
   span_button2.textContent = 'Download';
-  span_a1.href = front_end_books[i].link;
+  span_a1.href = front_end_books[i].imgPath;
   span_a1.target = '_blank';
   span_a2.href = front_end_books[i].link;
-  span_a2.download = front_end_books[i].link;
+  span_a2.target='_blank';
 }
 // back end book
 
@@ -98,10 +100,10 @@ for (let i = 0; i < 5; i++) {
   book_img.src = back_end_books[i].image;
   book_a.href = back_end_books[i].imgPath;
   book_a.target = '_blank';
-  span_button1.textContent = 'Read';
+  span_button1.textContent = 'Description';
   span_button2.textContent = 'Download';
-  span_a1.href = back_end_books[i].link;
+  span_button2.id='ahmed';
+  span_a1.href = back_end_books[i].imgPath;
   span_a1.target = '_blank';
   span_a2.href = back_end_books[i].link;
-  span_a2.download = back_end_books[i].link;
 }
