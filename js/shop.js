4d2a7e18f8e5557af18be7c3fbf48265ff6da6c6
m.zool@outlook.com <m.zool@outlook.com>
2021-03-03 14:41:51 +0200

    iam done

--- js/shop.js
+++ js/shop.js
@@ -28,3 +28,45 @@ function cartC() {
 if (newcart.allbooksAdded.length) {
   cartC();
 }
+
+const uniqueIDArr2 = [
+  'bt_0',
+  'bt_1',
+  'bt_2',
+  'bt_3',
+  'bt_4',
+  'bt_5',
+  'bt_6',
+  'bt_7',
+  'bt_8',
+  'bt_9',
+  'bt_10',
+  'bt_11',
+  'bt_12',
+  'bt_13',
+  'bt_14',
+  'bt_15',
+  'bt_16',
+  'bt_17',
+  'bt_18',
+  'bt_19',
+  'bt_20',
+  'bt_21',
+  'bt_22',
+  'bt_23',
+  'bt_24',
+];
+
+
+
+Container.addEventListener('click', clickHandler2);
+function clickHandler2(event) {
+  event.preventDefault();
+  for (let i = 0; i < 25; i++) {
+    if (event.target.id === uniqueIDArr2[i]) {
+      localStorage.setItem('book_index' , i);
+      document.location.href = '../details.html';
+    }
+  }
+
+}
